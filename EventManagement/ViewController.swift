5d6bc6585f4e3fc610c8a331d09eac504e0ddcbd
tanhien0809 <hien@Hiens-Mac.local>
2017-04-27 21:44:17 +0700

    Up

--- EventManagement/ViewController.swift
+++ EventManagement/ViewController.swift
@@ -9,10 +9,19 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    @IBOutlet dynamic weak var Day: UILabel!
+    @IBOutlet dynamic weak var NameEvent: UILabel!
+    @IBOutlet dynamic weak var Detail: UILabel!
+    var Day_Of_Week = String()
+    var Name_Event = String()
+    var Detail_Event = String()
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+        Day.text = Day_Of_Week
+        NameEvent.text = Name_Event
+        Detail.text = Detail_Event
     }
 
     override func didReceiveMemoryWarning() {
